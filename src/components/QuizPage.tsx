d368fe27ea2ddc0797d2547047deafd5f33b3778
Luca Becker <luca.dev@outlook.de>
2024-11-30 21:32:02 +0100

    Small bug fixes
    Signed-off-by: Luca Becker <luca.dev@outlook.de>

--- src/components/QuizPage.tsx
+++ src/components/QuizPage.tsx
@@ -19,6 +19,8 @@ const QuizPage: React.FC = () => {
   } = useQuizStore();
 
   const [feedback, setFeedback] = useState<string | null>(null);
+  const [buttonText, setButtonText] = useState("Überprüfen");
+  const [areButtonsDisabled, setAreButtonsDisabled] = useState(false);
 
   const handleSubmit = () => {
     if (selectedAnswers.size === 0) {
@@ -29,10 +31,15 @@ const QuizPage: React.FC = () => {
       return;
     }
     checkAnswer(setFeedback);
-    setTimeout(() => {
-      setFeedback(null);
-      nextQuestion();
-    }, 2000);
+    setButtonText("Next");
+    setAreButtonsDisabled(true);
+  };
+
+  const handleNext = () => {
+    nextQuestion();
+    setButtonText("Überprüfen");
+    setFeedback(null);
+    setAreButtonsDisabled(false);
   };
 
   const handleBackToHome = () => {
@@ -43,6 +50,24 @@ const QuizPage: React.FC = () => {
   return (
     <div className="flex justify-center items-center bg-gray-100 p-8">
       <div className="bg-white p-8 rounded-lg shadow-lg max-w-xl w-full">
+        {/* Progress Bar */}
+        {!showResults && (
+          <div className="mb-4">
+            <p className="text-center mb-1">
+              Frage {currentQuestionIndex + 1} von {questions.length}
+            </p>
+            <div className="w-full bg-gray-200 rounded-full h-4">
+              <div
+                className="bg-blue-600 h-4 rounded-full"
+                style={{
+                  width: `${
+                    ((currentQuestionIndex + 1) / questions.length) * 100
+                  }%`,
+                }}
+              ></div>
+            </div>
+          </div>
+        )}
         {!showResults ? (
           <motion.div
             animate={{ opacity: 1, y: 0 }}
@@ -68,6 +93,7 @@ const QuizPage: React.FC = () => {
                   onClick={() => toggleAnswer(oIndex)}
                   whileHover={{ scale: 1.05 }}
                   whileTap={{ scale: 0.95 }}
+                  disabled={areButtonsDisabled}
                 >
                   {option}
                 </motion.button>
@@ -97,33 +123,45 @@ const QuizPage: React.FC = () => {
               </motion.div>
             )}
             <motion.button
-              onClick={handleSubmit}
+              onClick={buttonText === "Überprüfen" ? handleSubmit : handleNext}
               className="mt-4 bg-blue-600 text-white px-6 py-2 rounded-full w-full hover:bg-blue-700"
               whileHover={{ scale: 1.05 }}
               whileTap={{ scale: 0.95 }}
             >
-              Überprüfen
+              {buttonText}
             </motion.button>
           </motion.div>
         ) : (
           <div>
             <h2 className="text-xl font-semibold mb-4 text-center">
-              Ihr Ergebnis: {score}
+              Ihr Ergebnis: {score}/ 12
             </h2>
             {scoreRanges.map(
               (
                 range: { minScore: number; maxScore: number; message: string },
                 index: number
-              ) =>
-                score >= range.minScore && score <= range.maxScore ? (
-                  <p key={index} className="text-lg text-center">
+              ) => {
+                const isLowerRange = index < 2;
+                const isMiddleRange = index === 2;
+                const bgColor = isLowerRange
+                  ? "bg-red-200 text-red-800"
+                  : isMiddleRange
+                  ? "bg-yellow-200 text-yellow-800"
+                  : "bg-green-200 text-green-800";
+
+                return score >= range.minScore && score <= range.maxScore ? (
+                  <p
+                    key={index}
+                    className={`text-lg text-center p-4 rounded ${bgColor}`}
+                  >
                     {range.message}
                   </p>
-                ) : null
+                ) : null;
+              }
             )}
             <button
               onClick={handleBackToHome}
-              className="mt-4 bg-gray-600 text-white px-6 py-2 rounded-full w-full hover:bg-gray-700"
+              className="mt-4 bg-gray-600 text-white px-6 py-2 rounded-lg w-full hover:bg-gray-700"
             >
               Zurück zur Startseite
             </button>
